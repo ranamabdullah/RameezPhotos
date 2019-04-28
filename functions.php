5f20cb76575d94814fdc1959907f1c5cb1744200
Nadeem Shah <nadeem.abbas.shah90@gmail.com>
2019-04-29 01:43:20 +0500

    menu navigation related functionality added

--- functions.php
+++ functions.php
@@ -18,7 +18,6 @@ function theme_scripts() {
 	wp_enqueue_script( 'custom', get_theme_file_uri( 'js/custom.js' ), array(), '1.0', true );
 
 }
-add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
 function learningWordPress_resources() {
 	
@@ -26,7 +25,6 @@ function learningWordPress_resources() {
 	
 }
 
-add_action('wp_enqueue_scripts', 'learningWordPress_resources');
 
 
 
@@ -60,8 +58,31 @@ function custom_excerpt_length() {
 	return 22;
 }
 
-add_filter('excerpt_length', 'custom_excerpt_length');
 
+// function insert_category_and_attach_to_menu($title,$slug,$menu_header_id){
+// 	$term = wp_insert_term(	$title, 'category',array('slug' => $slug));
+// 	if (!is_wp_error($term)){
+// 		error_log("wp_update_nav_menu_item");
+// 		$term_id = $term['term_id'];
+// 		wp_update_nav_menu_item($menu_header_id, 0, array(
+// 			'menu-item-title' => $title,
+// 			'menu-item-url' => get_category_link($term_id),
+// 			'menu-item-status' => 'publish',
+// 			'menu-item-object' => 'category',
+// 			'menu-item-parent-id' => 0)
+// 		);
+// 	}
+// }
+
+function attach_page_to_menu($page_title,$page_id,$menu_id){
+	wp_update_nav_menu_item($menu_id, 0, 
+      array(  'menu-item-title' => $page_title,
+             'menu-item-object' => 'page',
+             'menu-item-object-id' => $page_id,
+             'menu-item-type' => 'post_type',
+             'menu-item-status' => 'publish',
+             'menu-item-parent-id' => 0));
+}
 
 
 // Theme setup
@@ -72,6 +93,38 @@ function learningWordPress_setup() {
 		'primary' => __( 'Primary Menu'),
 		'footer' => __( 'Footer Menu'),
 	));
+
+	//get 'your_custom_menu' id to assign it to the primary menu location created
+	$menu_header = get_term_by('name', 'rameez_photos_menu', 'nav_menu');
+	$menu_header_id = $menu_header->term_id;
+
+	//if menu not found, create a new one
+	if($menu_header_id == 0) {
+		$menu_header_id = wp_create_nav_menu('rameez_photos_menu');
+	}
+
+	//Get all locations (including the one we just created above)
+	$locations = get_theme_mod('nav_menu_locations');
+
+	//set the menu to the new location and save into database
+	$locations['primary'] = $menu_header_id;
+	set_theme_mod( 'nav_menu_locations', $locations );
+
+	// Add Categories
+	// insert_category_and_attach_to_menu('Video Editing','video-editing',$menu_header_id);
+
+	// insert_category_and_attach_to_menu('Logo Design','logo-design',$menu_header_id);
+
+	// insert_category_and_attach_to_menu('Web Design','web-design',$menu_header_id);
+
+	// insert_category_and_attach_to_menu('Photo Editing','photo-editing',$menu_header_id);
+
+	// Add Pages
+	add_home_and_attach_to_menu('Home',$menu_header_id);
+	add_page_and_attach_to_menu("Portfolio",$menu_header_id);
+	add_page_and_attach_to_menu("Events",$menu_header_id);
+	add_page_and_attach_to_menu("About Us",$menu_header_id);
+	add_page_and_attach_to_menu("Contact Us",$menu_header_id);
 
 	// Add featured image support
 	add_theme_support('post-thumbnails');
@@ -84,8 +137,61 @@ function learningWordPress_setup() {
 
 }
 
+function add_home_and_attach_to_menu($title,$menu_id){
+	if (isset($_GET['activated']) && is_admin()){
+		wp_update_nav_menu_item($menu_id, 0, array(
+			'menu-item-title' => $title,
+			'menu-item-status' => 'publish',
+			'menu-item-url' => get_home_url(),
+			'menu-item-parent-id' => 0)
+		);
+	}
+}
 
-add_action('after_setup_theme', 'learningWordPress_setup');
+function add_page_and_attach_to_menu($page_title,$menu_id){
+
+	$page_id = add_page($page_title);
+	if($page_id != -1){
+		attach_page_to_menu($page_title,$page_id,$menu_id);
+	}
+}
+
+function add_page($title){
+	$new_page_id = -1;
+	if (isset($_GET['activated']) && is_admin()){
+	error_log("add_page ". $title);
+  
+		$new_page_title = $title;
+		$new_page_content = '';
+		$new_page_template = ''; //ex. template-custom.php. Leave blank if you don't want a custom page template.
+	  
+		//don't change the code bellow, unless you know what you're doing
+	  
+		$page_check = get_page_by_title($new_page_title);
+		$new_page = array(
+			'post_type' => 'page',
+			'post_title' => $new_page_title,
+			'post_content' => $new_page_content,
+			'post_status' => 'publish',
+			'post_author' => 1,
+		);
+		error_log($page_check->ID);
+
+		if(!isset($page_check->ID)){
+			error_log("page_check false");
+			$new_page_id = wp_insert_post($new_page);
+			if(!empty($new_page_template)){
+				update_post_meta($new_page_id, '_wp_page_template', $new_page_template);
+			}
+		}
+	}
+	return $new_page_id;
+
+}
+
+
+
+
 
 // Add Widget Areas
 function ourWidgetsInit() {
@@ -137,7 +243,6 @@ function ourWidgetsInit() {
 	
 }
 
-add_action('widgets_init', 'ourWidgetsInit');
 
 
 // Customize Appearance Options
@@ -183,7 +288,6 @@ function learningWordPress_customize_register( $wp_customize ) {
 
 }
 
-add_action('customize_register', 'learningWordPress_customize_register');
 
 
 
@@ -220,11 +324,46 @@ function learningWordPress_customize_css() { ?>
 
 <?php }
 
+function theme_deactivation_function(){
+	wp_delete_nav_menu('rameez_photos_menu'); 
+
+	$page = get_page_by_title( 'Home' );
+	wp_delete_post($page->ID,true);
+	$page = get_page_by_title( 'Portfolio');
+	wp_delete_post($page->ID,true);
+	$page = get_page_by_title( 'Events');
+	wp_delete_post($page->ID,true );
+	$page = get_page_by_title( 'About Us' );
+	wp_delete_post($page->ID, true);
+	$page = get_page_by_title( 'Contact Us');
+	wp_delete_post($page->ID, true);
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+
+add_action('wp_enqueue_scripts', 'learningWordPress_resources');
+
 add_action('wp_head', 'learningWordPress_customize_css');
 
+add_action('customize_register', 'learningWordPress_customize_register');
 
-// Add Categories
-wp_insert_term(	'Photo Editing', 'category',array('slug' => 'photo-editing'));
-wp_insert_term(	'Video Editing', 'category',array('slug' => 'video-editing'));
-wp_insert_term(	'Logo Design', 'category',array('slug' => 'logo-design'));
-wp_insert_term(	'Web Design', 'category',array('slug' => 'web-design'));
+add_action('widgets_init', 'ourWidgetsInit');
+
+add_filter('excerpt_length', 'custom_excerpt_length');
+
+// action hook when theme activated
+add_action('after_setup_theme', 'learningWordPress_setup');
+
+// action hook when theme deactivated
+add_action('switch_theme', 'theme_deactivation_function');
+
+
+// function rameez_photos_after_setup_theme(){
+// 	// wp_die("enough is enough");
+// 	error_log( "enough is enough" );
+
+// }
+
+// add_action( 'after_switch_theme', 'rameez_photos_after_setup_theme' );
+
